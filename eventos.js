60656f650f8a44db65a78334c35b95480fad0cd7
GabrielNole <nolegabriel@hotmail.com>
2023-08-05 16:56:16 -0300

    ultimos cambios

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const divContenedor = document.getElementById("divContenedor");
+    divContenedor.addEventListener("click", function () {
+        alert("Hola! Soy el div");
+    });
+});
